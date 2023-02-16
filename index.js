0c97a44ceb4285860177ac814855cbfd9e551a57
Ivan Rocha <ivan.rocha.0987@gmail.com>
2023-02-16 17:15:24 -0300

    :tada: initial commit

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function toggleMode() {
-  alert('oi')
+  const html = document.documentElement
+
+  html.classList.toggle('light')
+
+  const image = document.querySelector("#profile img")
+
+  if(html.classList.contains('light')) {
+    image.setAttribute("src", "./image/Avatar.png")
+  } else {
+    image.setAttribute("src", "./image/Avatar-light.png")
+  }
 }
