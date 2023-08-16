c8ffc286c0c390ba0cb5db81f3b10669b10b2138
Ivan Rocha <ivan.rocha.0987@gmail.com>
2023-08-16 18:30:11 -0300

    Commit

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ function toggleMode() {
 
   html.classList.toggle('light')
 
-  const image = document.querySelector("#profile img")
+  const image = document.querySelector('#profile img')
 
-  if(html.classList.contains('light')) {
-    image.setAttribute("src", "../image/Avatar.png")
+  if (html.classList.contains('light')) {
+    image.setAttribute('src', '../image/Avatar.png')
   } else {
-    image.setAttribute("src", "../image/Avatar-light.png")
+    image.setAttribute('src', '../image/Avatar-light.png')
   }
 }
